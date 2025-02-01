77d06a4a5a124f385a6e8112d147770d66a46d37
CodeSpace <CodeSpace@FRANK-REC>
2025-02-01 14:22:20 +0800

    Add project files.

---
+++ AIA_Phil/wwwroot/js/site.js
@@ -0,0 +1,14 @@
+﻿// Write your JavaScript code.
+function toggleSidebar() {
+    var sidebar = document.getElementById('sidebar');
+    var content = document.getElementById('content');
+    var sidebarLeft = window.getComputedStyle(sidebar).getPropertyValue('left');
+
+    if (sidebarLeft === '0px') {
+        sidebar.style.left = '-270px';
+        content.style.marginLeft = '0';
+    } else {
+        sidebar.style.left = '0';
+        content.style.marginLeft = '270px';
+    }
+}
